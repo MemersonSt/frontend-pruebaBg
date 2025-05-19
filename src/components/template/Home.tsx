cbe0aac56dfe684e2b7bcb6d32ca47cf9987d17c
MemersonSt <emerson.cajape@tecsu.edu.ec>
2025-05-19 17:29:54 -0500

    commit end

--- src/components/template/Home.tsx
+++ src/components/template/Home.tsx
@@ -2,17 +2,37 @@ import { Card, CardContent, Grid2, Typography } from "@mui/material";
 import useProducts from "../../hooks/useProducts";
 import useAuth from "../../hooks/useAuth";
 import Transition from "../ui/Transition";
+import { useMemo } from "react";
+// import type { IProductsPrice } from "../../interface/IProductsPrice";
 
 export default function HomeTemplate() {
   const { products } = useProducts();
   const { user } = useAuth();
+
+  const productsList = useMemo(() => {
+    const data = products.map((m) => {
+      let totalPrice: number = 0;
+      m.prices?.forEach((f) => {
+        totalPrice = totalPrice + f.price;
+      });
+
+      const prom = totalPrice / (m.prices?.length ?? 0);
+
+      return {
+        ...m,
+        price: prom,
+      };
+    });
+
+    return data;
+  }, [products]);
 
   return (
     <Transition>
       <h1>Bienvenido {user?.name}</h1>
       <p>Estos son los productos disponibles:</p>
       <Grid2 container spacing={2}>
-        {products.map((product) => (
+        {productsList.map((product) => (
           <Grid2 key={product.code} size={{ xs: 6, md: 3 }}>
             <Card>
               <CardContent>
@@ -24,6 +44,9 @@ export default function HomeTemplate() {
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                   Categoría: {product.category}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Precio: {product.price}
                 </Typography>
               </CardContent>
             </Card>
